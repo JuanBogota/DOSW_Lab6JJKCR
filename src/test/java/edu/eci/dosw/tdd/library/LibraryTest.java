a8e0fca3f2ed10ea9e85844becc0b3d10d7de5d4
unknown <juan.bogota-f@labinfo.is.escuelaing.edu.co>
2026-03-07 11:20:28 -0500

    Feat: Se realizó la creación de la clase LibraryTest y se corrigió la estructura

--- src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -3,7 +3,7 @@ package edu.eci.dosw.tdd;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AppTest {
+public class LibraryTest {
 
     @Test
     public void shouldAnswerWithTrue() {
